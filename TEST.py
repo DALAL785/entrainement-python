48638a7213718aec7c7f8636d81cdc254815280f
abdelj78 <abdel78500@outlook.fr>
2023-03-08 19:31:54 +0000

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -2,8 +2,18 @@ def main():
    # print("abdeljabbaar le fou ")
     print("je suis grave concentrer")
 
+def calculmoyenne(val1,val2):
+    total=val1+val2
+    resultat=total/2
+    return resultat
+
+
 if __name__ == '__main__':
- main()
+    main()
+    x=5
+    y=8
+    moyenne=calculmoyenne(x,y)
+    print(moyenne)
 
 
 
