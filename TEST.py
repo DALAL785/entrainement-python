84d3f061e7eaf2ae539b77cda15fc6b69eca3cab
DALAL785 <127342997+DALAL785@users.noreply.github.com>
2023-03-09 23:06:23 +0100

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,33 +1,18 @@
-def main():
-    # creation d'une variable usernam ayant pour valeur le mot abdel
-    usernam = " abdel"
-    # creation d'une variable age ayant pour valeur 19
-    age =  19
-    print("salam" +  str( usernam) + " tu as " + str( age) + " ans")
-    # le porte monnaie d'une personne
-    wallet = 125.7
-    print("Mon compte bancaire est de  " + str(wallet) + " euros")
-    # Je vais acheter des bonbons d'une valeur de 3e
-    bonbons = 3
-    print("bonbons -"  + str(bonbons ) + "euros " )
-    wallet = (wallet- bonbons)
-    print("Votre solde est de " + str(wallet))
-    print(bonbons < 2)
 
+# On indique la somme deposé
+somme_depose = 100
+valeur_bit = 19500
 
-    print("Mon compte bancaire est de  " + str(wallet)  + " euros")
-    # Maintenant abdel jvai te montrer que je sais faire des moyennes
-    valeur1 = int(input("Entrer la valeur 1"))
-    valeur2 = int(input("Entrer la valeur 2"))
-    valeur3 = int(input("Entrer la valeur 3"))
-    valeur4 = int(input("Entrer la valeur 4"))
-    resultat = (valeur1 + valeur2 + valeur3 + valeur4) / 4
-    print("La moyenne est de " + str(resultat))
+valeur_eur =(somme_depose/valeur_bit)
+print("solde de depart " + str(valeur_eur)+ " bitcoin")
 
+valeur_bit_actuelle = int(input("Notez la valeur actuelle du Bitcoin"))
 
-if __name__ == '__main__':
-    main()
+calcul_pourcentage = ((valeur_bit_actuelle/valeur_bit) - 1) * 100
 
-
+if calcul_pourcentage > 0 :
+    print("La valeur du BTC à évolué de " + str(calcul_pourcentage) + "%")
+else:
+    print("La valeur du BTC à baissé de " + str(calcul_pourcentage) + "%")
 
 
