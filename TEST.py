41e87591c1e4dac36e4a8455fd61a20d1ed193ca
DALAL785 <127342997+DALAL785@users.noreply.github.com>
2023-03-09 20:24:39 +0100

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,19 +1,32 @@
 def main():
-   # print("abdeljabbaar le fou ")
-    print("je suis grave concentrer")
+    # creation d'une variable usernam ayant pour valeur le mot abdel
+    usernam = " abdel"
+    # creation d'une variable age ayant pour valeur 19
+    age =  19
+    print("salam" +  str( usernam) + " tu as " + str( age) + " ans")
+    # le porte monnaie d'une personne
+    wallet = 125.7
+    print("Mon compte bancaire est de  " + str(wallet) + " euros")
+    # Je vais acheter des bonbons d'une valeur de 3e
+    bonbons = 3
+    print("bonbons -"  + str(bonbons ) + "euros " )
+    wallet = (wallet- bonbons)
+    print("Votre solde est de " + str(wallet))
+    print(bonbons < 2)
 
-def calculmoyenne(val1,val2):
-    total=val1+val2
-    resultat=total/2
-    return resultat
+
+    print("Mon compte bancaire est de  " + str(wallet)  + " euros")
+    # Maintenant abdel jvai te montrer que je sais faire des moyennes
+    valeur1 = int(input("Entrer la valeur 1"))
+    valeur2 = int(input("Entrer la valeur 2"))
+    valeur3 = int(input("Entrer la valeur 3"))
+    valeur4 = int(input("Entrer la valeur 4"))
+    resultat = (valeur1 + valeur2 + valeur3 + valeur4) / 4
+    print("La moyenne est de " + str(resultat))
 
 
 if __name__ == '__main__':
     main()
-    x=5
-    y=67
-    moyenne=calculmoyenne(x,y)
-    print(moyenne)
 
 
 
