77f1656a57e456b5add1acd51ae442b41837eca9
abdelj78 <abdel78500@outlook.fr>
2023-03-11 23:28:37 +0000

    Update calculateur benef crypto.py

--- calculateur benef crypto.py
+++ calculateur benef crypto.py
@@ -11,10 +11,10 @@ valeur_eth = 915.24
 valeur_doge = 0.076
 
 #nb de crypto acheté
-valeur_eur_bit = round(somme_depose_bit/valeur_bit, 2)
-valeur_eur_xpr = round(somme_depose_xpr/valeur_xpr, 2)
-valeur_eur_eth = round(somme_depose_eth/valeur_eth, 2)
-valeur_eur_doge = round(somme_depose_doge/valeur_doge, 2)
+valeur_eur_bit = somme_depose_bit/valeur_bit
+valeur_eur_xpr = somme_depose_xpr/valeur_xpr
+valeur_eur_eth = somme_depose_eth/valeur_eth 
+valeur_eur_doge =somme_depose_doge/valeur_doge
 print("vous avez actuellement:")
 print("BTC: "+str(valeur_eur_bit))
 print("XRP: "+str(valeur_eur_xpr))
@@ -27,11 +27,10 @@ valeur_xpr_actuelle = float(input("Notez la valeur actuelle du XRP  "))
 valeur_eth_actuelle = float(input("Notez la valeur actuelle du ETH  "))
 valeur_doge_actuelle = float(input("Notez la valeur actuelle du DOGE    "))
 
-calcul_pourcentage_bit = round(((valeur_bit_actuelle/valeur_bit) - 1) * 100, 2)
-calcul_pourcentage_xpr = round(((valeur_xpr_actuelle/valeur_xpr) - 1) * 100, 2)
-calcul_pourcentage_eth = round(((valeur_eth_actuelle/valeur_eth) - 1) * 100, 2)
-calcul_pourcentage_doge = round(((valeur_doge_actuelle/valeur_doge) - 1) * 100, 2)
-
+calcul_pourcentage_bit = ((valeur_bit_actuelle/valeur_bit) - 1) * 100
+calcul_pourcentage_xpr = ((valeur_xpr_actuelle/valeur_xpr) - 1) * 100
+calcul_pourcentage_eth = ((valeur_eth_actuelle/valeur_eth) - 1) * 100
+calcul_pourcentage_doge = ((valeur_doge_actuelle/valeur_doge) - 1) * 100
 
 
 benef_bit = (valeur_eur_bit*valeur_bit_actuelle) - somme_depose_bit
@@ -42,23 +41,22 @@ benef_doge = (valeur_eur_doge*valeur_doge_actuelle) - somme_depose_doge
 benefname=["BTC", "XRP" , "ETH", "DOGE"]
 benef=[benef_bit, benef_xpr, benef_eth, benef_doge]
 
-benef_total = round((benef_doge+benef_eth+benef_xpr+benef_bit), 2)
+benef_total = (benef_doge+benef_eth+benef_xpr+benef_bit) 
 
-
-for x in range(0, len(benef)-1):
-    
-    if x > 0 :
-        print("La valeur du "+ benefname[x] + " à évolué de " + str(benef[x]) + " %")
-    elif x == 0:
+for x in range(0, len(benef)):
+  
+    if benef[x] > 0 :
+        print("La valeur du "+ benefname[x] + " à évolué de " + str(round(benef[x],2)) + " %")
+    elif benef[x] == 0:
         print("La valeur du "+benefname[x] + " est resté la meme")
     else:
-        print("La valeur du "+benefname[x] +" à baissé de " + str(benef[x]) + " %")
+        print("La valeur du "+benefname[x] +" à baissé de " + str(round(benef[x],2) )+ " %")
 
 
 
 if benef_total > 0 :
-    print("Votre benefice est " + str(benef_total)+ " euros")
+    print("Votre benefice est " + str(round(benef_total,2))+ " euros")
 elif benef_total == 0:
     print("Votre benefice est nulle ")
 else :
-    print("Votre perte est " + str(benef_total)+ " euros")
+    print("Votre perte est " + str(round(benef_total,2))+ " euros")
